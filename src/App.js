02502489f371f850690188a0b7634650e91421b3
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-04-04 05:09:52 +0600

    added deshboard first chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import CustomerReviews from './components/CustomerReviews/CustomerReviews';
+import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NoRoute from './components/NoRoute/NoRoute';
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/customer-reviews" element={<CustomerReviews />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="*" element={<NoRoute />} />
         </Routes>
       </main>
