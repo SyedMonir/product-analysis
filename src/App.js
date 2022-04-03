18bad173008088bd3bba13580d7f357afd77aa3e
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-04-04 00:54:03 +0600

    added header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <>
+      <Navbar id="menubar" bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Games Breakdown</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <NavLink
+                to="/home"
+                className={({ isActive }) =>
+                  isActive ? 'active-route' : 'route'
+                }
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/reviews"
+                className={({ isActive }) =>
+                  isActive ? 'active-route' : 'route'
+                }
+              >
+                Reviews
+              </NavLink>
+              <NavLink
+                to="/dashboard"
+                className={({ isActive }) =>
+                  isActive ? 'active-route' : 'route'
+                }
+              >
+                Dashboard
+              </NavLink>
+              <NavLink
+                to="/blogs"
+                className={({ isActive }) =>
+                  isActive ? 'active-route' : 'route'
+                }
+              >
+                Blogs
+              </NavLink>
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  isActive ? 'active-route' : 'route'
+                }
+              >
+                About
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
